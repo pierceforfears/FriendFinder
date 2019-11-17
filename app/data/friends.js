dc01d12f8ae5dc9078e425fca2d7afda8ce021e8
Robert Pierce <pierce1673@gmail.com>
2019-11-17 09:51:04 -0500

    completed routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,20 @@
-const possibleFriends = [];
+const possibleFriends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
 
 module.exports = possibleFriends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = function (app) {
     // displays survey page
-    app.get("survey", function (req, res) {
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
     // catch-all displaying home.html
